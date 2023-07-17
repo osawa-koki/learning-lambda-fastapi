0998b799688bc54dcabef5d60499508cd2412586
osawa-koki <github@osawa.dev>
2023-07-17 15:34:21 +0900

    GET API

--- hello_world/app.py
+++ hello_world/app.py
@@ -3,8 +3,20 @@ from mangum import Mangum
 
 app = FastAPI()
 
-@app.get("/hello", status_code=200)
+@app.get("/", status_code=200)
 async def root():
-  return {"message": "Hello SAM World"}
+  return {"message": "Hello Get."}
+
+# @app.post("/", status_code=200)
+# async def root():
+#   return {"message": "Hello POST."}
+
+# @app.put("/", status_code=200)
+# async def root():
+#   return {"message": "Hello PUT."}
+
+# @app.delete("/", status_code=200)
+# async def root():
+#   return {"message": "Hello DELETE."}
 
 lambda_handler = Mangum(app)
