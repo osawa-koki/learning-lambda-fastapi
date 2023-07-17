13e53512ee50e8b551cad274befb420efc88c02c
osawa-koki <github@osawa.dev>
2023-07-17 19:11:21 +0900

    GET, POST, PUT, DELETE

--- hello_world/app.py
+++ hello_world/app.py
@@ -7,16 +7,16 @@ app = FastAPI()
 async def root():
   return {"message": "Hello Get."}
 
-# @app.post("/", status_code=200)
-# async def root():
-#   return {"message": "Hello POST."}
+@app.post("/", status_code=200)
+async def root():
+  return {"message": "Hello POST."}
 
-# @app.put("/", status_code=200)
-# async def root():
-#   return {"message": "Hello PUT."}
+@app.put("/", status_code=200)
+async def root():
+  return {"message": "Hello PUT."}
 
-# @app.delete("/", status_code=200)
-# async def root():
-#   return {"message": "Hello DELETE."}
+@app.delete("/", status_code=200)
+async def root():
+  return {"message": "Hello DELETE."}
 
 lambda_handler = Mangum(app)
